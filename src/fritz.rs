8e3a4496127f711b6b1c077919419bee08dbde5b
lukasjoc <45790416+lukasjoc@users.noreply.github.com>
2024-12-31 12:44:23 +0100

    feat: fritz cmd wip

--- src/fritz.rs
+++ src/fritz.rs
@@ -1,16 +1,22 @@
-use dotenv::dotenv;
-use form_urlencoded;
 use md5::{Digest, Md5};
 use serde::{Deserialize, Serialize};
-use std::env;
+use std::fs;
 
 #[derive(clap::Parser)]
 struct Args {}
 
 #[derive(clap::Subcommand)]
 enum Commands {
+    /// Initialize the config for the tool automatically.
+    Init(Args),
     /// Display debugging information about the FritzBox.
     Info(Args),
+    /// Reboot the device instantly.
+    Reboot(Args),
+    /// Reconnect the device, usually with a new IP.
+    Reconnect(Args),
+    /// List known devices on the network.
+    Devices(Args),
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq, Default)]
@@ -23,28 +29,35 @@ struct SessionInfo {
     block_time: i64,
 }
 
-#[derive(Debug)]
-struct FritzClient<'a> {
-    base_url: &'a str,
-    session_info: SessionInfo,
+#[derive(Debug, Serialize, Deserialize)]
+struct Config {
+    base_url: String,
+    username: String,
+    password: String,
 }
 
-impl<'a> FritzClient<'a> {
-    fn authenticate(base_url: &'a str, username: &str, password: &str) -> reqwest::Result<Self> {
+#[derive(Debug)]
+struct FritzApi {
+    config: Config,
+    session_info: SessionInfo,
+    client: reqwest::blocking::Client,
+}
+
+impl FritzApi {
+    fn new_authenticate(config: Config) -> reqwest::Result<Self> {
         let client = reqwest::blocking::Client::new();
 
         // getting the challenge data
         let challenge_response = client
-            .get(format!("{base_url}/login_sid.lua"))
+            .get(format!("{base}/login_sid.lua", base = config.base_url))
             .send()?
             .text()?;
         let info: SessionInfo =
             serde_xml_rs::from_str(&challenge_response).expect("Should get the session info");
-        //
 
         // Prepare the response data payload for the login request
         let mut buf: Vec<u8> = vec![];
-        for char in (format!("{}-{}", info.challenge, password)).encode_utf16() {
+        for char in (format!("{}-{}", info.challenge, config.password)).encode_utf16() {
             let ch = match char {
                 ch if ch > 255 => 0x2e,
                 other => other,
@@ -55,22 +68,22 @@ impl<'a> FritzClient<'a> {
 
         let mut hasher = Md5::new();
         hasher.update(&buf);
-        let sum: String = hasher
+        let sum = hasher
             .finalize()
             .to_vec()
             .iter()
-            .map(|x| format!("{:02x}", x))
-            .collect();
+            .map(|b| format!("{:02x}", b))
+            .collect::<String>();
 
-        let body_payload: String = form_urlencoded::Serializer::new(String::new())
-            .append_pair("username", username)
+        let payload = form_urlencoded::Serializer::new(String::new())
+            .append_pair("username", &config.username)
             .append_pair("response", &format!("{}-{}", info.challenge, sum))
             .finish();
 
         let auth_response = client
-            .post(format!("{base_url}/login_sid.lua"))
+            .post(format!("{base}/login_sid.lua", base = config.base_url))
             .header("Content-Type", "application/x-www-form-urlencoded")
-            .body(body_payload)
+            .body(payload)
             .send()?
             .text()?;
 
@@ -79,8 +92,9 @@ impl<'a> FritzClient<'a> {
 
         // TODO: reject with error if sids of challenge response and auth_response match.
         Ok(Self {
-            base_url,
+            config,
             session_info,
+            client,
         })
     }
 }
@@ -90,24 +104,20 @@ pub(crate) struct Cli {
     #[command(subcommand)]
     command: Option<Commands>,
 }
-
 impl Cli {
-    fn info(&self, _args: &Args) -> reqwest::Result<()> {
-        todo!()
+    fn info(&self, api: &FritzApi, _args: &Args) -> reqwest::Result<()> {
+        let sid = &api.session_info.sid;
+        println!("SID: {sid}");
+        Ok(())
     }
     pub(crate) fn run(&self) -> reqwest::Result<()> {
-        dotenv().ok();
-        let base_url = env::var("BASE_URL").expect("Need env var.");
-        let username = env::var("USERNAME").expect("Need env var.");
-        let password = env::var("PASSWORD").expect("Need env var.");
-
-        let client = FritzClient::authenticate(&base_url, &username, &password)?;
-        println!("BASE_URL: {:?}", client.base_url);
-        println!("INFO: {:?}", client.session_info);
-
+        let file = fs::File::open("./config.json").unwrap();
+        let config: Config = serde_json::from_reader(file).unwrap();
+        let api = FritzApi::new_authenticate(config)?;
         if let Some(command) = &self.command {
             match command {
-                Commands::Info(args) => self.info(args)?,
+                Commands::Info(args) => self.info(&api, args)?,
+                _ => unimplemented!("Command not implemented yet!"),
             }
         }
         Ok(())
